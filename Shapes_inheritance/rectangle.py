d10e11e9da3f121d0b6b1345d377ac558413538a
BLAKEJEWELL <bjewell1833@gmail.com>
2026-03-26 11:21:41 -0400

    step 3 looks done

---
+++ Shapes_inheritance/rectangle.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod #might not need this
+import math
+from tkinter import W
+from Basicshape import basicshape
+
+class rectangle(basicshape):
+    def __init__(self,l,w):
+        self._length = float(l)
+        self._width = float(w)
+        self.calc_area()
+
+    def calc_area(self):
+        self._area = self._lenth * self._width
+
+    @property
+    def length(self):
+        return self._length
+    @property
+    def width(self):
+        return self._width
+    @length.setter
+    def length(self, val):
+        self._length = val
+        self.calc_area()
+    @width.setter
+    def width(self, val):
+        self._width = val
+        self.calc_area()
+
+
+
+
