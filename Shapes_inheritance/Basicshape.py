d95f5e4ab6e0f0bbcd036cf823a77a6f2f4050df
BLAKEJEWELL <bjewell1833@gmail.com>
2026-03-26 10:33:42 -0400

    step 1 complete. nothing works.

--- Shapes_inheritance/Basicshape.py
+++ Shapes_inheritance/Basicshape.py
@@ -1,6 +1,29 @@
-class basicshape():
-    pass
+from abc import ABC, abstractmethod
+import math
 
+class basicshape(ABC):
+    def __init__(self):
+        self._area = 0.0    #place holder
+        self._name = "name" #place holder
 
+    @property
+    def get_area(self):
+        return self._area
+
+    @property
+    def Get_area(self):
+        return self._name
+
+    @area.setter
+    def name(self):     #ATTENTION
+        self._area = 
+
+    @name.setter
+    def name(self):     #ATTENTION
+        self._name = 
+
+    @abstarctmethod
+    def calc_area(self):
+        pass
 
 
